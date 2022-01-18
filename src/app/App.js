8514c80495556cea61a28445c02a76390832b31f
Michael Clark <michael.clark1992@gmail.com>
2022-01-18 16:05:43 -0600

    install deps, setup provider

--- src/app/App.js
+++ src/app/App.js
@@ -1,11 +1,19 @@
+import { Web3ReactProvider } from "@web3-react/core";
+import { Web3Provider } from "@ethersproject/providers";
 import "./App.scss";
+
+const getLibrary = (provider, connector) => {
+  return new Web3Provider(provider);
+};
 
 function App() {
   return (
-    <div className="App">
-      <h1>Gas Station</h1>
-      <p>How much am I paying in transaction fees this week?</p>
-    </div>
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <div className="App">
+        <h1>Gas Station</h1>
+        <p>How much am I paying in transaction fees this week?</p>
+      </div>
+    </Web3ReactProvider>
   );
 }
 
