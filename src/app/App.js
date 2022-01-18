ee2908f976c540189650f82eb48caf2d317a4d7b
Michael Clark <michael.clark1992@gmail.com>
2022-01-18 15:59:08 -0600

    added node-sass, setup files

---
+++ src/app/App.js
@@ -0,0 +1,12 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Gas Station</h1>
+      <p>How much am I paying in transaction fees this week?</p>
+    </div>
+  );
+}
+
+export default App;
